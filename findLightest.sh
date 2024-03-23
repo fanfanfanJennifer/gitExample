64b5456f1b85eaefd1e7ab253909674975eed2e0
Yifan Zhang <116487190+fanfanfanJennifer@users.noreply.github.com>
2024-03-23 01:42:23 -0500

    Update findLightest.sh

--- findLightest.sh
+++ findLightest.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-
-cat out_csv/out1.csv out_csv/out2.csv out_csv/out3.csv > combined_output.csv
-lightest=$(awk 'NR == 1 || $1 < min { min = $1 } END { print min }' combined_output.csv)
-echo $lightest > out
-
+awk '{print $1}' out1.csv out2.csv out3.csv | sort -n | head -n1 > out
